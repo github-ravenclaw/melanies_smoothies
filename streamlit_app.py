9c60602916e4ca3ade64632f29a5c40e4bd12abf
github-ravenclaw <64080582+github-ravenclaw@users.noreply.github.com>
2025-07-10 12:36:39 +0530

    Update streamlit_app.py (7)

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ st.stop()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
-    max_selections=5
+    max_selections=6
 )
 
 # If user selected ingredients
@@ -37,10 +37,10 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     # Trim trailing space
